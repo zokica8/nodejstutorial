d7a18b86da74228d111c96cad041d61fc4fe0ca5
zokiv <zokivasilic8@gmail.com>
2020-05-28 14:07:43 +0200

    identity_test

---
+++ 08-identity_test_operator.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// identity test operator ===, and equality test operator ==
+let number = 10;
+
+console.log('number == 10', number == '10');
+console.log('number === 10', number === '10');
+
+// != and !==
+console.log('number != 10', number != '10');
+console.log('number !== 10', number !== '10');
